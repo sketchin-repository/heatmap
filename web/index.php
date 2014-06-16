32c624221e3c02b2de29792ebeaba05644a339c0
lasavino <padawan@Padawan.local>
2014-06-16 15:06:52 +0200

    Configurato template engine

--- web/index.php
+++ web/index.php
@@ -3,8 +3,14 @@ require_once __DIR__.'/../vendor/autoload.php';
 
 $app = new Silex\Application();
 
-$app->get('/', function () {
-    return 'Heatmap';
+$app->register(new Silex\Provider\TwigServiceProvider(), array(
+    'twig.path' => __DIR__.'/views',
+));
+
+$app->get('/', function () use ($app) {
+    return $app['twig']->render('index.html', array(
+        'name' => "laura",
+    ));
 });
 
 $app->run();
