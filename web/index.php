d0089e826e69d86153dfe187b8edc8e260c346f2
lasavino <padawan@Padawan.local>
2014-06-26 09:34:25 +0200

    Geocoding funzionante
    Salva i risultati su file json

--- web/index.php
+++ web/index.php
@@ -11,68 +11,6 @@ use Symfony\Component\Validator\Mapping\ClassMetadata;
 use Symfony\Component\HttpKernel\HttpKernelInterface;
 
 /**
-* Structure definition
-*/
-class structure {
-	// properties
-	public $name;
-    public $lon;
-    public $lat ;
-    public $count;
-         
-    // constructor
-    public function __construct($a, $b, $c, $d) {
-    	$this->name = $a;
-        $this->lon = $b;
-        $this->lat = $c;
-        $this->count = $d;
-        
-    }
-}
-
-/**
-* Geocoding function
-*/
-function custom_geocoding($file_path) {
-
-    $csvFile = new Keboola\Csv\CsvFile($file_path, ",");
-    $dati_strutturati = array();
-    //$cols_names[] = $csvFile->getHeader();
-
-    $geocoder = new \Geocoder\Geocoder();
-    $adapter = new \Geocoder\HttpAdapter\SocketHttpAdapter();
-
-    //$apiKey = 'AIzaSyDTXsR4Id1Hk4xowxa9yCKeEn1Q6C2buEo';
-
-    $provider = new \Geocoder\Provider\GoogleMapsProvider($adapter);//, null, null, true, $apiKey);
-    $geocoder->registerProvider($provider);
-
-    $geocoded = new \Geocoder\Result\Geocoded();
-
-    foreach($csvFile as $row) {
-
-        // 2000000 = 2sec
-        usleep(200);
-
-        try {
-            $geocoded = $geocoder->geocode($row[0]);
-
-            $dati_strutturati[] = new structure($row[0], $geocoded->getLongitude(), $geocoded->getLatitude(), $row[1]);
-
-        } catch (Exception $e) {
-            echo $e->getMessage();
-        }
-    }
-
-    return $dati_strutturati; // è un array di structure (oggetto definito da utente)
-
-    $fp = fopen('assets/data/data.json', 'w');
-    fwrite($fp, json_encode($dati_strutturati));
-    fclose($fp);
-}
-
-
-/**
 * Create app
 */
 require_once __DIR__.'/../vendor/autoload.php';
@@ -194,6 +132,7 @@ $app->match('/add/02', function (Request $request) use ($app) {
         $data = $optionsForm->getData();
         
         // array(4) { ["heatmapName"]=> string(6) "Poppa!" ["cityNameColumn"]=> int(0) ["valueColumn"]=> int(1) ["valueFormat"]=> int(0) }
+        
         $computedData = array();
         $rawData = array();
         
@@ -209,7 +148,7 @@ $app->match('/add/02', function (Request $request) use ($app) {
             } else if ($data["valueFormat"]==1) {
                 $tempComputedRow['value'] = (float)$row[$data["valueColumn"]];
             }
-            
+
             $computedData[] = $tempComputedRow;
             
             // Dati grezzi
@@ -221,6 +160,15 @@ $app->match('/add/02', function (Request $request) use ($app) {
             
             $rawData[] = $tempRawRow;
         }
+
+        array_shift($computedData);
+        array_shift($rawData);
+
+        // Carica i dati computati in sessione
+        $app['session']->set('newData', array(
+            'data' => $computedData, 
+            'userFileName' => $data["heatmapName"]
+            ));
         
         return $app['twig']->render('add_02_preview.html', array(
                 'form' => $optionsForm->createView(),
@@ -232,6 +180,67 @@ $app->match('/add/02', function (Request $request) use ($app) {
     return $app['twig']->render('add_02.html', array(
             'form' => $optionsForm->createView(),
             'table' => $heatmapData['analyticsData']
+        ));
+});
+
+$app->match('/add/03', function (Request $request) use ($app) {
+    
+    if (null === $newData = $app['session']->get('newData')) {
+        return $app->redirect('/add/01');
+    }
+
+
+    // Geocoding
+
+    $geocoder = new \Geocoder\Geocoder();
+    $adapter = new \Geocoder\HttpAdapter\SocketHttpAdapter();
+
+    //$apiKey = 'AIzaSyDTXsR4Id1Hk4xowxa9yCKeEn1Q6C2buEo';
+
+    $provider = new \Geocoder\Provider\GoogleMapsProvider($adapter);//, null, null, true, $apiKey);
+    $geocoder->registerProvider($provider);
+
+    $tmpGeocoded = new \Geocoder\Result\Geocoded();
+
+
+    $geocodedData = array();
+
+    
+    foreach($newData['data'] as $row) {
+
+        $tmpGeocodedRow = array(
+        "city" => "",
+        "value" => "",
+        "longitude" => "null",
+        "latitude" => "null",
+        "result" => "success"
+        );
+
+        $tmpGeocodedRow["city"] = $row["city"];
+        $tmpGeocodedRow["value"] = $row["value"];
+
+        try {
+            $tmpGeocoded = $geocoder->geocode($row["city"]);
+            
+            $tmpGeocodedRow["longitude"] = $tmpGeocoded->getLatitude();
+            $tmpGeocodedRow["latitude"] = $tmpGeocoded->getLongitude();
+
+        } catch (Exception $e) {
+            $tmpGeocodedRow["result"] = $e->getMessage();
+        }
+
+         $geocodedData[] = $tmpGeocodedRow;
+    }
+
+    $jsontoHeat = fopen($newData['userFileName'], 'w');
+    fwrite($jsontoHeat, json_encode($geocodedData, JSON_PRETTY_PRINT));
+    fclose($jsontoHeat);
+
+
+
+    // Presenta la tabella geocodata (city - value - lat - lng - result)
+    return $app['twig']->render('add_03.html', array(
+        'geocoded' => $geocodedData,
         ));
 });
 
